2f93aec582b60a275a8886e84fbd2b2c7e966036
Ryan Siegelberg <160647148+Ryanblockz@users.noreply.github.com>
2024-03-17 22:21:48 +0200

    yolo

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+console.log("WHAT THE FUCK ;(");
+
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+
+const incrementButton = document.getElementById("increment-btn")
+const saveButton = document.getElementById("save-btn")
 
 function increment() {
     count += 1
@@ -13,3 +18,6 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
+incrementButton.addEventListener("click", increment);
+saveButton.addEventListener("click", save);
